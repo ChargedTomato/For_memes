20a7040ab2ef0cbee2133dca711cdb1dc888610c
ChargedTomato <dd4756029@gmail.com>
2023-09-25 22:38:17 +0400

    1. Посмотрел как вводить данные 2. Познакомился с условными апператорами

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,35 @@
 package com.company;
-import java.util.Scanner;
+import java.util.Scanner; // слово Import помогает импортировать
 public class Main {
 
     public static void main(String[] args) {
 	    int x;
-        Scanner console = new Scanner(System.in);
+        String y;
+        Scanner console = new Scanner(System.in); // java.util.Scanner(позволяет вносить данные)
+
+        System.out.print("Your age:");
         x = console.nextInt();
-        System.out.println(x);
+
+        System.out.print("Your name:");
+        y = console.next();
+
+        System.out.printf("Your age is %d Your name is %s \n", x, y);
+        console.close();
+
+       if ( x >= 18)
+       {
+           System.out.print("Beer Tine");
+       }
+       else if (x > 0 && x < 18)
+       {
+           while (x < 18)
+           {
+               System.out.println("Прошёл год");
+               x = x+1;
+           }
+           System.out.print("BEER TIME!");
+       }
+       else
+           System.out.print("Wat");
     }
 }
